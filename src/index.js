1d66ccaa62e4b0c4332a5659ab549e2021ba028e
Marco Frisan <ender.saka@gmail.com>
2017-12-07 14:18:39 +0100

    Installed Hogan, Request and Browserify configuration.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+//import * as hogan from 'hogan';
+//import * as request from 'request';
+
+var hogan = require('hogan.js');
+var request = require('request');
+
+console.log('Hogan ', hogan);
+console.log('Require ', require);
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: '/Users/VR User/Documents/Sorgenti/apps/marco_frisan/europass_wolverine/dist'
+  }
+}
