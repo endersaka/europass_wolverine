0351602e4d2dd198c101bd8834c992a6be8297b8
Marco Frisan <ender.saka@gmail.com>
2017-12-07 15:28:21 +0100

    Implemented sending XML to Europass via REST and receiving JSON.

--- src/index.js
+++ src/index.js
@@ -5,4 +5,67 @@ var hogan = require('hogan.js');
 var request = require('request');
 
 console.log('Hogan ', hogan);
-console.log('Require ', require);
+console.log('Request ', require);
+
+function europassJSONReceived(json, response) {
+  var jsonCV = JSON.parse(json);
+  console.log('jsonCV ', jsonCV);
+}
+
+function htmlTemplateReceived(html, response) {
+  console.log('html template ', html);
+}
+
+var jsonCallback = function (error, response, content) {
+  if (error) {
+    console.log('error ', error);
+    return null;
+  } else {
+    if (response && response.statusCode === 200) {
+      console.log('json: ', content);
+      console.log('response: ', response);
+      var contentType = response.headers['content-type'].split(';')[0];
+      switch (contentType) {
+        case 'application/json':
+          europassJSONReceived(content, response);
+          break;
+        case 'application/xml':
+          htmlTemplateReceived(content, response);
+          break;
+        default:
+
+      }
+
+
+    } else {
+      console.log('statusCode: ', response.statusCode);
+    }
+  }
+}
+
+var requestEuropasXML = request(
+  'http://localhost:8080/CV-Europass-20170407-IT.xml',
+  function (error, response, body) {
+    if (error) {
+      console.log('error ', error);
+      return null;
+    } else {
+      if (response && response.statusCode === 200) {
+        //console.log('body: ', body);
+
+        var jsonOptions = {
+          url: 'https://europass.cedefop.europa.eu/rest/v1/document/to/json',
+          headers: {
+            'Content-Type': 'application/xml'
+          },
+          body: body
+        };
+
+        var requestEuropassJSON = request.post(jsonOptions, jsonCallback);
+      } else {
+        console.log('statusCode: ', response.statusCode);
+      }
+    }
+
+  }
+);
